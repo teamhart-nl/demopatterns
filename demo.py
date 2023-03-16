f355aa8c3c8992528ec69a31fe0b61f4f29ce2f5
CocoTheOwner <sjoerdvdgoor@hotmail.com>
2023-03-16 13:56:38 +0100

    demo script

---
+++ demo.py
@@ -0,0 +1,9 @@
+### Description ###
+# This python script contains a small demonstration game
+# The game is one where you learn three patterns and then
+# have to guess which pattern is the correct one.
+# The game is played in the terminal.
+###################
+
+
+
